f2cd666bb745ca980778c584a95f11f114d371ab
RAHAT <105932806+rahat2924@users.noreply.github.com>
2024-04-25 22:50:23 +0600

    Create .java

---
+++ .java
@@ -0,0 +1,19 @@
+ abstract class Vehicle {
+ public abstract void startEngine();
+ public abstract void stopEngine();
+ }
+ class Car extends Vehicle {
+ // Implement startEngine() and
+ stopEngine() for car
+ }
+ class Motorcycle extends Vehicle {
+ // Implement startEngine() and
+ stopEngine() for motorcycle
+ }
+ public class Main {
+ public static void main(String[]
+ args) {
+ // Code to test Car and
+ Motorcycle classes
+ }
+ }
